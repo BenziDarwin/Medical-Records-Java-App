54040d23fa35ff9dfb62c74f6aa6ad28263d6bec
Ssali Benjamin <ssalibenjamin0402@gmail.com>
2022-11-11 21:24:01 +0300

    First commit

---
+++ src/village/clinic/records/VillageClinicRecords.java
@@ -0,0 +1,18 @@
+package village.clinic.records;
+
+/**
+ *
+ * @author ejer
+ */
+public class VillageClinicRecords {
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.ClassNotFoundException
+     * @throws java.lang.InstantiationException
+     * @throws javax.swing.UnsupportedLookAndFeelException
+     */
+    public static void main(String[] args){
+        
+        new Login().setVisible(true);
+    }   
+}
